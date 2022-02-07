4dac368ec3c96aa371d6edb5544be15811c2c864
Arttu Pennanen <arttu@pennanen.org>
2022-02-07 18:20:19 +0200

    style: remove redundant console.info

--- modules/location/index.js
+++ modules/location/index.js
@@ -6,11 +6,9 @@ function getCurrentLocation() {
     console.info('trying to get the current location')
     return new Promise((resolve, reject) => {
         function onSuccess(position) {
-            console.info('got location', position.coords.latitude, position.coords.longitude)
             resolve(position)
         }
         function onError(error) {
-            console.error('failed to get the location', error)
             reject(error)
         }
 
