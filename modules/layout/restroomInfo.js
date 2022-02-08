3b2830cee5144cabc3aeaa4e007760d6d43fef2e
Arttu Pennanen <arttu@pennanen.org>
2022-02-09 01:22:51 +0200

    style: add semicolons to headings

--- modules/layout/restroomInfo.js
+++ modules/layout/restroomInfo.js
@@ -6,11 +6,9 @@ export async function setRestroomElement(target, data) {
     const container = document.createElement('div')
     container.className = 'info-container'
 
-    console.log(data.tags)
-
     data.tags.name && container.appendChild(createPart({ heading: data.tags.name }))
-    container.appendChild(createPart({ heading: 'Osoite', text: data.streetName }))
-    container.appendChild(createPart({ heading: 'Lisätty', text: dateToFinnishLocale(new Date(data.timestamp)) }))
+    container.appendChild(createPart({ heading: 'Osoite:', text: data.streetName }))
+    container.appendChild(createPart({ heading: 'Lisätty:', text: dateToFinnishLocale(new Date(data.timestamp)) }))
     for (const [key, value] of Object.entries(data.tags)) {
         container.appendChild(createPart({ heading: key + ':', text: value }))
     }
