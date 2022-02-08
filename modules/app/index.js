bff7f61a5eb7231c33901e2bc026b3c2643bb5fa
Arttu Pennanen <arttu@pennanen.org>
2022-02-08 03:15:24 +0200

    feat!: add app

---
+++ modules/app/index.js
@@ -0,0 +1,156 @@
+import * as L from '../../vendor/leaflet/leaflet-src.esm.js'
+import Polyline from '../../vendor/polyline/index.js'
+import { setRouteInfoElement } from '../layout/routeInfo.js'
+import { setRestroomElement } from '../layout/restroomInfo.js'
+import { getHSLRoute } from '../api/hsl/routing.js'
+
+const redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+})
+
+export class App {
+    constructor({ location: { lat, lon }, restrooms }) {
+        if (!document.getElementById('map')) throw new Error('Page does not have an element with the id of "map"')
+
+        this.location = { lat, lon }
+        this.routes = new Map()
+        this.restrooms = new Map()
+
+        this.addRestrooms(restrooms)
+
+        this.map = L.map('map').setView({ lat, lon }, 13)
+        this.locationMarker = L.marker(this.location, { icon: redIcon }).addTo(this.map).addTo(this.map)
+        this.currentRoute
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map)
+    }
+
+    static calculateDistance(a, b) {
+        return Math.sqrt(Math.pow(b.lat - a.lat, 2) + Math.pow(b.lon - a.lon, 2))
+    }
+
+    static locationsEqual(a, b) {
+        if (a.lat === b.lat && b.lon === a.lon) {
+            return true
+        }
+        return false
+    }
+
+    async getRoute(id) {
+        const restroom = this.restrooms.get(id)
+        if (!restroom) throw new Error('Restroom was not stored in memory')
+        const storedRoute = this.routes.get(id)
+
+        if (storedRoute && App.locationsEqual(storedRoute.from, this.location)) {
+            return storedRoute.value
+        }
+
+        const updatedRoute = await getHSLRoute({
+            from: this.location,
+            to: restroom.location
+        })
+
+        this.routes.set(id, {
+            from: this.location,
+            value: updatedRoute
+        })
+
+        return updatedRoute
+    }
+
+    async showRoute(id) {
+        const restroom = this.restrooms.get(id)
+        setRestroomElement(document.querySelector('.app-restroom-info'), restroom)
+        const route = await this.getRoute(id)
+        setRouteInfoElement(document.querySelector('.app-route-info'), route)
+
+        for (const leg of route.legs) {
+            const points = leg.legGeometry.points
+            const decoded = Polyline.decode(points)
+
+            if (this.currentRoute) {
+                this.map.removeLayer(this.currentRoute)
+            }
+            this.currentRoute = L.polyline(decoded)
+                .setStyle({
+                    color: 'blue'
+                })
+                .addTo(this.map)
+        }
+        return route
+    }
+
+    showRestrooms() {
+        for (const restroom of this.restrooms.values()) {
+            const marker = L.marker(restroom.location).addTo(this.map)
+            marker.on('click', () => this.showRoute(restroom.id))
+        }
+    }
+
+    addRestrooms(restrooms) {
+        for (const restroom of restrooms) {
+            if (this.restrooms.has(restroom.id)) continue
+            this.restrooms.set(restroom.id, {
+                ...restroom,
+                distance: {
+                    from: this.location,
+                    value: App.calculateDistance(this.location, restroom.location)
+                }
+            })
+        }
+    }
+
+    findDistance(id) {
+        const restroom = this.restrooms.get(id)
+        if (!restroom) throw new Error('Restroom was not stored in memory')
+        if (App.locationsEqual(restroom.distance.from, this.location)) {
+            return restroom.distance.value
+        }
+
+        const updatedRestroom = {
+            ...restroom,
+            distance: {
+                from: this.location,
+                value: App.calculateDistance(this.location, restroom.location)
+            }
+        }
+        this.restrooms.set(id, updatedRestroom)
+        return updatedRestroom.distance.value
+    }
+
+    findClosest() {
+        let closestRestroom
+        let closestDistance
+
+        this.restrooms.forEach((restroom) => {
+            const restroomDistance = this.findDistance(restroom.id)
+            if (!closestRestroom || restroomDistance < closestDistance) {
+                closestRestroom = restroom
+                closestDistance = restroomDistance
+                return
+            }
+        })
+
+        return closestRestroom
+    }
+}
+
+// export async function appPollingFunction(app) {
+//     const newLocation = await getCurrentLocation()
+//     const lat = newLocation.coords.latitude
+//     const lon = newLocation.coords.longitude
+//     const latChange = Math.abs(lat - app.location.lat)
+//     const lonChange = Math.abs(lon - app.location.lon)
+//     if (latChange > 0.0001 || lonChange > 0.0001) {
+//         console.log('updating')
+//         app.updateLocation({ lat: newLocation.coords.latitude, lon: newLocation.coords.longitude })
+//     } else {
+//         console.log('change too litle')
+//     }
+// }
---
+++ modules/tests/createApp.js
@@ -0,0 +1,22 @@
+import { App } from '../app/index.js'
+
+import { fakeLocation, fakeRestrooms } from '../util/fakedata.js'
+
+async function test() {
+    if (!document.getElementById('map')) throw new Error('Page does not have an element with the id of "map"')
+    // const location = await getCurrentLocation()
+    const location = fakeLocation
+    // const restrooms = await getRestrooms({ lat: location.coords.latitude, lon: location.coords.longitude })
+    const restrooms = fakeRestrooms
+
+    document.getElementById('loading-spinner').classList.add('hidden')
+
+    const app = new App({ location: { lat: location.coords.latitude, lon: location.coords.longitude }, restrooms })
+
+    app.showRestrooms()
+
+    const closest = app.findClosest()
+    app.showRoute(closest.id)
+}
+
+test()
