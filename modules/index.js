278036e99fa8ff3ee2c763ca6a77ca16306998cf
Arttu Pennanen <arttu@pennanen.org>
2022-02-14 19:49:59 +0200

    refactor: main functionality in new location

---
+++ modules/index.js
@@ -0,0 +1,57 @@
+import { App } from './app/index.js'
+
+async function test() {
+    if (!document.getElementById('map')) throw new Error('Page does not have an element with the id of "map"')
+
+    let location
+
+    const cachedLocation = App.getStoredLocation()
+    if (cachedLocation) {
+        location = cachedLocation
+    } else {
+        location = await App.fetchLocation()
+    }
+
+    document.getElementById('loading-spinner').classList.add('hidden')
+    const app = new App({ location })
+
+    app.setVisible()
+
+    let restrooms
+
+    const cachedRestrooms = App.getStoredRestrooms()
+    if (cachedRestrooms) {
+        restrooms = cachedRestrooms
+    } else {
+        restrooms = await App.fetchRestroomsFromLocation(location)
+    }
+
+    app.addRestrooms(restrooms)
+
+    const closest = app.getClosestRestroom()
+    app.showRouteToRestroom(closest.id)
+
+    // Update app every 35 seconds
+    setInterval(() => {
+        app.updateApp()
+    }, 1000 * 35)
+
+    app.updateApp()
+
+    // window.app = app
+    // window.App = App
+
+    document.querySelector('#focususer').addEventListener('click', () => {
+        app.setViewUserLocation()
+    })
+
+    document.querySelector('#focusroute').addEventListener('click', () => {
+        app.fitMapToLocations(app.location, app.selectedRestroom.location)
+    })
+
+    document.querySelector('#routenearest').addEventListener('click', () => {
+        app.showRouteToRestroom(app.getClosestRestroom().id)
+    })
+}
+
+test()
