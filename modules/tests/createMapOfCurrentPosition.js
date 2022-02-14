8ac17c90ecaf62bf2576ca66a5555109ca6374e6
Arttu Pennanen <arttu@pennanen.org>
2022-02-14 19:49:26 +0200

    chore: remove old tests

--- modules/tests/createMapOfCurrentPosition.js
+++
@@ -1,8 +0,0 @@
-import { getCurrentLocation } from '../location/index.js'
-import { getMap } from '../map/index.js'
-
-// Creates a map around the device location
-
-getCurrentLocation()
-    .then((location) => getMap({ lat: location.coords.latitude, lon: location.coords.longitude }))
-    .then((map) => console.info(map))
