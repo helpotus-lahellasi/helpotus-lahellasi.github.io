d657f0b7a55be6e78cf16678d81227156b9b3a1b
Arttu Pennanen <arttu@pennanen.org>
2022-02-14 19:51:02 +0200

    refactor: getStreetName to API folder

---
+++ modules/api/routereverse/streetNameFromPosition.js
@@ -0,0 +1,6 @@
+export async function getStreetName(lat, lon) {
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}%2C${lon}&key=f39175ee4fad4dfa952c4f207a3667ab&language=fi&pretty=1`
+    const res = await fetch(url)
+    const data = await res.json()
+    return data.results[0].formatted
+}
