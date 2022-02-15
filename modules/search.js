cd46c9146bf6349c4d279b27204f4d7886515107
Tuomo <90772011+motski@users.noreply.github.com>
2022-02-15 14:52:41 +0200

    Hakusivutestings + css Co-authored-by: Arttu Pennanen <Pennane@users.noreply.github.com>

--- modules/search.js
+++ modules/search.js
@@ -1,16 +1,36 @@
 import { getSearch } from './api/osm/search.js'
+import { App } from './app/index.js'
+import { setSearchResultsElement } from './layout/searchResults.js'
 
 
-let timeout
-
-function onSearch(event) {
-    clearTimeout(timeout)
-    timeout = setTimeout(() => {
-        console.log(event.target.value)
-        getSearch(event.target.value);
-    }, 500)
+async function getRestrooms(location) {
+    return await App.fetchRestroomsFromLocation(location)
 }
 
+async function main() {
 
-document.querySelector('#searchbar').addEventListener('change', onSearch);
-document.querySelector('#searchbar').addEventListener('keyup', onSearch);
+    function onClick(event, data) {
+        ///
+        console.log(data)
+
+    }
+
+    let timeout
+
+    function onSearch(event) {
+        clearTimeout(timeout)
+        timeout = setTimeout(async() => {
+            console.log(event.target.value)
+            const data = await getSearch(event.target.value);
+            setSearchResultsElement(document.querySelector('.search-results'), data, onClick)
+        }, 500)
+
+    }
+
+
+
+    document.querySelector('#searchbar').addEventListener('change', onSearch);
+    document.querySelector('#searchbar').addEventListener('keyup', onSearch);
+}
+
+main()
