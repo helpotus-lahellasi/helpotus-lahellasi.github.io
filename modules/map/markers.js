1f70fd63bd824bbbafc94badadbc230cbc04f795
Tuomo <90772011+motski@users.noreply.github.com>
2022-02-16 14:04:17 +0200

    markers.js icon changes

--- modules/map/markers.js
+++ modules/map/markers.js
@@ -1,7 +1,7 @@
 import * as L from '../../vendor/leaflet/leaflet-src.esm.js'
 
 const iconBase = {
-    iconSize: [25, 41],
+    iconSize: [32, 32],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
@@ -10,7 +10,22 @@ const iconBase = {
 export const icons = {
     red: new L.Icon({
         ...iconBase,
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        iconUrl: 'https://pennane.github.io/web-tekniikat-projekti/icons/favicon-32x32.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    }),
+    black: new L.Icon({
+        ...iconBase,
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    }),
+    green: new L.Icon({
+        ...iconBase,
+        iconUrl: 'https://pennane.github.io/web-tekniikat-projekti/icons/favicon-32x32.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    }),
+    gold: new L.Icon({
+        ...iconBase,
+        iconUrl: 'https://pennane.github.io/web-tekniikat-projekti/icons/favicon-32x32.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     })
 
