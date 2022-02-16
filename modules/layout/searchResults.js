2e7a56b4d5f02375656414e7c1aabd4bd82071d7
Tuomo <90772011+motski@users.noreply.github.com>
2022-02-16 17:50:34 +0200

    Haku CSS
    Co-authored-by: Arttu Pennanen <Pennane@users.noreply.github.com>

--- modules/layout/searchResults.js
+++ modules/layout/searchResults.js
@@ -4,13 +4,18 @@ export async function setSearchResultsElement(target, data, onClickHandler) {
     clearElement(target)
 
     const container = document.createElement('div')
-    container.className = 'info-container'
+    container.className = 'searchresults-container'
+
+    console.log(data)
 
     for (const location of data) {
         const button = document.createElement('button')
         button.addEventListener('click', (event) => onClickHandler(event, location))
         button.className = 'search-result-button'
-        const element = createPart({ heading: 'Osoite:', text: location.display_name })
+        const heading = location.display_name.substring(0, location.display_name.indexOf(', '))
+        const rest = location.display_name.substring(location.display_name.indexOf(', ') + 2)
+
+        const element = createPart({ heading: heading, text: rest })
         button.appendChild(element)
         container.appendChild(button)
         target.appendChild(container)
