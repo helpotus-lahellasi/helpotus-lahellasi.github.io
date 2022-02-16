b4fe3703bebf142e7102172e441eaba6589108ee
Tuomo <90772011+motski@users.noreply.github.com>
2022-02-16 14:04:33 +0200

    deleted unnamed road

--- modules/api/routereverse/streetNameFromPosition.js
+++ modules/api/routereverse/streetNameFromPosition.js
@@ -8,5 +8,11 @@ export async function getStreetName(lat, lon) {
     const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}%2C${lon}&key=f39175ee4fad4dfa952c4f207a3667ab&language=fi&pretty=1`
     const res = await fetch(url)
     const data = await res.json()
-    return data.results[0].formatted
+    const routename = data.results[0].formatted
+
+    if (routename.startsWith("unnamed road,")) {
+        return routename.substring("unnamed road,".length)
+    }
+
+    return routename
 }
