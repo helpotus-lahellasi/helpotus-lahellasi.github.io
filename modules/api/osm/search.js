1ee9898f0809fd22d1632ffd70fdbe14a66e146c
Arttu Pennanen <39066923+pennane@users.noreply.github.com>
2024-01-26 17:49:50 +0200

    add missing q=

--- modules/api/osm/search.js
+++ modules/api/osm/search.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://nominatim.openstreetmap.org/search'
 export async function getSearch(searchString) {
     const params = `format=json&addressdetails=1&limit=4`
-    const url = `${baseUrl}?${encodeURIComponent(searchString)}&${params}`
+    const url = `${baseUrl}?q=${encodeURIComponent(searchString)}&${params}`
     const res = await fetch(url)
     const data = await res.json()
     return data
